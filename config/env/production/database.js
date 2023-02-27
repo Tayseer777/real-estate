ba22a3a6d882b620301582d07471361bae9a73e3
Tayseer777 <tayseer@vuedale.com>
2023-02-27 16:41:01 +0300

    VV

---
+++ config/env/production/database.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'postgres');
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'real_state'),
+        user: env('DATABASE_USERNAME', 'vuedale'),
+        password: env('DATABASE_PASSWORD', 'pbAza2En8bvbJZCXxBbmIYmwvMjL7np1'),
+        schema: env('DATABASE_SCHEMA', 'public'),
+      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    },
+  };
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
